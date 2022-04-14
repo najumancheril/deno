53d8f6b5bd5e99034f014d9564f50e37ee7ff1a7
Naju Mancheril <naju.mancheril@gmail.com>
2022-04-14 02:26:33 -0400

    now using file fetcher to load eval files

--- cli/main.rs
+++ cli/main.rs
@@ -919,7 +919,16 @@ async fn repl_command(
   }
   worker.run_event_loop(false).await?;
 
-  tools::repl::run(&ps, worker, repl_flags.eval_file, repl_flags.eval).await
+  let eval_file_config = match repl_flags.eval_file {
+    Some(eval_file) => Some(tools::repl::EvalFileConfig::new(
+      vec![eval_file],
+      !ps.flags.no_remote,
+      ps.flags.unsafely_ignore_certificate_errors.clone(),
+    )?),
+    None => None,
+  };
+
+  tools::repl::run(&ps, worker, eval_file_config, repl_flags.eval).await
 }
 
 async fn run_from_stdin(flags: Flags) -> Result<i32, AnyError> {
